58d853493945aa95e9de8751f545b7603b1b5f3d
Lukasz Kunicki <lkunicki@gravityglobal.com>
2021-08-15 12:01:40 +0200

    Some server logic prepared, gitignore modified to include .env file

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApiInstance = new SpotifyWebApi({
+    redirectUri: server.env.REDIRECT_URI,
+    clientId: server.env.CLIENT_ID,
+    clientSecret: server.env.CLIENT_SECRET
+  });
+
+  spotifyApiInstance
+  .authorizationCodeGrant(code)
+  .then((data) => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in
+    })
+  })
+  .catch(() => {
+    res.sendStatus(400);
+  })
+});
