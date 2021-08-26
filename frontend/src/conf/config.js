2af3a4cead4e6906faf61bce2000f195fa2ea655
Lukasz Kunicki <lkunicki@gravityglobal.com>
2021-08-26 21:55:38 +0200

    Change API URL

--- frontend/src/conf/config.js
+++ frontend/src/conf/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: "https://spotify-lyrics-web-app.herokuapp.com/",
+  API_URL: "https://spotify-lyrics-web-app.herokuapp.com",
   CLIENT_ID: "e2f5bc73916845cca657f51299b431a6",
   REDIRECT_URI: "https://spotify-lyrics-web-app.herokuapp.com",
 };
